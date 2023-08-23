35e3849c769c6fe533c3e5781cc3514c9e78aa94
ztreasure13 <141884744+ztreasure13@users.noreply.github.com>
2023-08-23 18:49:07 -0400

    Add files via upload

---
+++ GamesQuery1.sql
@@ -0,0 +1,24 @@
+INSERT INTO Games
+VALUES ('One Piece Odyssey', 'Role-playing', 'ILCA', 'Bandai Namco Entertainment', '2023-01-13'),
+	('Forspoken', 'Action role-playing', 'Luminous Productions', 'Square Enix', '2023-01-24'),
+	('Final Fantasy XVI', 'Action role-playing', 'Square Enix', 'Square Enix', '2023-06-22'),
+	('Hi-Fi Rush', 'Rhythm, Action', 'Tango Gameworks', 'Bethesda Softworks', '2023-01-25'),
+	('Street Fighter 6', 'Fighting', 'Capcom', 'Capcom', '2023-06-02'),
+	('Diablo IV', 'Action role-playing', 'Blizzard Entertainment', 'Blizzard Entertainment', '2023-06-06'),
+	('Lies of P', 'Action role-playing', 'Round8 Studio', 'Neowiz', '2023-09-19'),
+	('Mortal Kombat 1', 'Fighting', 'NetherRealm Studios', 'NetherRealm Studios', '2023-09-19'),
+	('Hogwarts Legacy', 'Action role-playing', 'Avalanche Software', 'Warner Bros. Games', '2023-02-10'),
+	('Fire Emblem Engage', 'Tactical role-playing', 'Intelligent Systems', 'Nintendo', '2023-01-20'),
+	('Pikmin 4', 'Real-time strategy', 'Nintendo EPD, Eighting', 'Nintendo', '2023-01-24'),
+	('Madden NFL 24', 'Sports', 'EA Tiburon', 'Electronic Arts', '2023-08-18'),
+	('NBA 2k24', 'Sports', 'Visual Concepts', '2k', '2023-09-08'),
+	('Disgaea 7: Vows of the Virtueless', 'Role-playing', 'Nippon Ichi Software', 'NIS America', '2023-10-03'),
+	('Lords of the Fallen', 'Action role-playing', 'Hexworks', 'CI Games', '2023-10-13'),
+	('Alan Wake II', 'Survivor horror', 'Remedy Entertainment', 'Epic Games Publishing', '2023-10-13'),
+	('Exoprimal', 'Third-person shooter', 'Capcom', 'Capcom', '2023-07-14'),
+	('Marvel''s Spider-Man 2', 'Role-playing', 'Nippon Ichi Software', 'NIS America', '2023-10-20'),
+	('Assassin''s Creed Mirage', 'Action-adventure', 'Ubisoft Bordeaux', 'Ubisoft', '2023-10-12'),
+	('Avatar: Frontiers of Pandora', 'Action-adventure', 'Massive Entertainment', 'Ubisoft', '2023-12-07')
+
+SELECT *
+FROM Games;
